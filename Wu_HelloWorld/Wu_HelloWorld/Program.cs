d0df438820adaf7f53a8cc99b293fbe394b8f4c7
Ming Wu <mw4086@rit.edu>
2021-08-25 17:10:48 -0400

    hello world

---
+++ Wu_HelloWorld/Wu_HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wu_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 0; 
+            Console.WriteLine("Hello Mingshen Wu!");
+            Console.WriteLine("What is your favorite number?");
+            favNumber = Convert.ToInt32(Console.ReadLine()) ;
+            Console.WriteLine("You enter the number " + favNumber + "as your favorite number! I love it!");
+        }
+    }
+}
