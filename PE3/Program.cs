679d51860010c5c149341bb07700735a7a6bf1a2
Ming Wu <mw4086@rit.edu>
2021-09-06 20:32:52 -0400

    PE4 question 2

--- PE3/Program.cs
+++ PE3/Program.cs
@@ -10,29 +10,7 @@ namespace PE3
     {
         static void Main(string[] args)
         {
-            string readInput = null;
-            int firstNumber = 0;
-            int secondNumber = 0;
-            int thirdNumber = 0;
-            int fourthNumber = 0;
-            Console.WriteLine("Please enter four Number");
-            Console.WriteLine("First Number:");
-            readInput = Console.ReadLine();
-            firstNumber = Convert.ToInt32(readInput);
-
-            Console.WriteLine("Second Number");
-            readInput = Console.ReadLine();
-            secondNumber = Convert.ToInt32(readInput);
-
-            Console.WriteLine("Third Number");
-            readInput = Console.ReadLine();
-            thirdNumber = Convert.ToInt32(readInput);
-
-            Console.WriteLine("Fourth Number");
-            readInput = Console.ReadLine();
-            fourthNumber = Convert.ToInt32(readInput);
-
-            Console.WriteLine("What you have entered are: " + firstNumber + ", " + secondNumber + ", " + thirdNumber + ", " + fourthNumber);
+            Console.Out.WriteLine("50 plus 25 is " + 50 + 25);
             Console.ReadLine();
         }
     }
---
+++ PE4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            method();
+        }
+        static void method()
+        {
+            int var1;
+            int var2;
+            bool result;
+
+            Console.WriteLine("Please enter first number");
+            var1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+            var2 = Convert.ToInt32(Console.ReadLine());
+
+            result = (var1 < 10 && var2 < 10);
+            if (result == true)
+            {
+                Console.WriteLine("You have entered " + var1 + " and " + var2);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please re-enter the correct number");
+                method();
+            }
+        }
+    }
+}
