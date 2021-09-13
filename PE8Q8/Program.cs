5e742a36da7a97d245f33c5dd9f611afcd4a9549
Ming Wu <mw4086@rit.edu>
2021-09-13 17:26:49 -0400

    no to yes

---
+++ PE8Q8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Q8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentance with No, I will change it to yes!");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(userInput.Replace("no", "yes"));
+            Console.ReadLine();
+        }
+    }
+}
