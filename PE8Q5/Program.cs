ad2e01f0e434c974190d429364b9cbcf4e734659
Ming Wu <mw4086@rit.edu>
2021-09-13 17:21:22 -0400

    Q7

---
+++ PE8Q5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,,] data = new double[630,630,630];
+            int i = 0;
+            for(double x = -1; x < 1; x += 0.1)
+            {
+                for(double y = 1; y < 4; y += 0.1)
+                {
+                    data[i, i, i] = (3 * y * y) + (2 * x) - 1;
+                    i++;
+                }
+            }
+            foreach(var item in data)
+            {
+                Array.ForEach(data, Console.WriteLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PE8Q7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Q7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rev;
+            Console.WriteLine("Please enter a word");
+            string userInput = Console.ReadLine();
+            rev = "";
+            Console.WriteLine("Value of given String is: {0}", userInput);
+            int i;
+            i = userInput.Length - 1;
+            while (i >= 0)
+            {
+                rev = rev + userInput[i];
+                i--;
+            }
+            Console.WriteLine("Reversed string is: {0}", rev);
+            Console.ReadLine();
+        }
+    }
+}
