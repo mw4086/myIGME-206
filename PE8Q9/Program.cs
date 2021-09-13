fa1b61033ebe3827a527441fe9e85f808cd8759b
Ming Wu <mw4086@rit.edu>
2021-09-13 17:32:46 -0400

    double quotes

---
+++ PE8Q9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Q9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentance then I will put double quotes around it!");
+            string playerInput = Console.ReadLine();
+
+            string[] words = playerInput.Split(' ');
+            string finalResult = string.Join(" ", words).Replace(" ", "\"");
+            Console.WriteLine(finalResult);
+            Console.ReadLine();
+        }
+    }
+}
