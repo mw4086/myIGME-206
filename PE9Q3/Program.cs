be0d2c7b4aa91b442895fc314b96970aeb223aa7
Ming Wu <mw4086@rit.edu>
2021-09-15 22:01:07 -0400

    Q3

---
+++ PE9Q3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9Q3
+{
+    class Program
+    {
+        delegate string HomeMadeReadLine();
+        public static String ReadFunction()
+        {
+            return Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            HomeMadeReadLine readline = ReadFunction;
+            Console.WriteLine("Please Enter Something Here...");
+            string read = readline();
+            Console.WriteLine("You entered: {0}", read);
+        }
+    }
+}
