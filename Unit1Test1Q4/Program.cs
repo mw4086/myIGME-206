73719c2d409c40f4819f23accc36b3a9db3ce384
Ming Wu <mw4086@rit.edu>
2021-09-26 13:56:31 -0400

    q2

---
+++ Unit1Test1Q4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit1Test1Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your question");
+            int chooseQuestion = Convert.ToInt32(Console.ReadLine());
+
+            switch (chooseQuestion)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+}
