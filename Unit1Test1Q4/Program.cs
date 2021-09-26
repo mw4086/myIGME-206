38aba291c37a3c2943cf53edfc13fd34cda85056
Ming Wu <mw4086@rit.edu>
2021-09-26 14:58:39 -0400

    add timer

--- Unit1Test1Q4/Program.cs
+++ Unit1Test1Q4/Program.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Unit1Test1Q4
 {
     class Program
     {
+        private static System.Timers.Timer aTimer;
+        private static bool timeUp;
+        private static string[] questions = { "What if your favorite color?", "What is the answer to life, the universe and everything?", "What is the airspeed velocity of an unladen swallow?" };
+        private static string[] answers = { "black", "42", "What do you mean? African or European swallow?" };
+        private static int chooseQuestion;
+        public static void setTimer()
+        {
+            timeUp = false;
+            aTimer = new System.Timers.Timer(5000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+        }
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            timeUp = true;
+            Console.WriteLine("Time's up!");
+            Console.WriteLine("The answer is: {0}", answers[chooseQuestion]);
+            Console.Write("Please press enter.");
+        }
         static void Main(string[] args)
         {
             Game();
@@ -16,66 +37,46 @@ namespace Unit1Test1Q4
         static void Game()
         {
             string playerAnswer;
-            Console.WriteLine("Choose your question");
-            int chooseQuestion = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Choose your question (1-3): ");
+            chooseQuestion = Convert.ToInt32(Console.ReadLine()) - 1;
 
-            switch (chooseQuestion)
+
+            Console.WriteLine("You have 5 seconds to answer the following question:");
+            Console.WriteLine(questions[chooseQuestion]);
+            setTimer();
+            playerAnswer = Console.ReadLine();
+            if (timeUp)
             {
-                case 1:
-                    Console.WriteLine("What if your favorite color?");
-                    playerAnswer = Console.ReadLine();
-                    if (playerAnswer == "black")
-                    {
-                        Console.WriteLine("Well Done!");
-                        PlayAgain();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wrong! The Answer is: black");
-                        PlayAgain();
-                    }
-                    break;
-                case 2:
-                    Console.WriteLine("What is the answer to life, the universe and everything?");
-                    playerAnswer = Console.ReadLine();
-                    if (playerAnswer == "42")
-                    {
-                        Console.WriteLine("Well Done!");
-                        PlayAgain();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wrong! The Answer is: 42");
-                        PlayAgain();
-                    }
-                    break;
-                case 3:
-                    Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
-                    playerAnswer = Console.ReadLine();
-                    if (playerAnswer == "What do you mean? African or European swallow?")
-                    {
-                        Console.WriteLine("Well Done!");
-                        PlayAgain();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wrong! The Answer is: what do you mean? African or European swallow?");
-                        PlayAgain();
-                    }
-                    break;
+                playerAnswer = "";
+            }
+            if (playerAnswer == answers[chooseQuestion])
+            {
+                Console.WriteLine("Well Done!");
+                PlayAgain();
+            }
+            else if (playerAnswer == "")
+            {
+                Console.WriteLine("The answer is: {0}", answers[chooseQuestion]);
+                PlayAgain();
+            }
+            else
+            {
+                Console.WriteLine("Wrong! The answer is: {0}", answers[chooseQuestion]);
+                PlayAgain();
             }
         }
 
         static void PlayAgain()
         {
+            aTimer.Stop();
             Console.WriteLine("Play again?");
             string playAgain = Console.ReadLine();
             playAgain.ToLower();
-            if(playAgain == "yes" || playAgain == "y")
+            if (playAgain == "yes" || playAgain == "y")
             {
                 Game();
             }
-            else if(playAgain == "no" || playAgain == "n")
+            else if (playAgain == "no" || playAgain == "n")
             {
                 Environment.Exit(0);
             }
