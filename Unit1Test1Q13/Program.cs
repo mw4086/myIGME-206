592de5cf059f31a88e5ce3558ecdc25587a18e26
Ming Wu <mw4086@rit.edu>
2021-09-26 15:43:00 -0400

    q13 start

---
+++ Unit1Test1Q13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit1Test1Q13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
