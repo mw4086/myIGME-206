662fc0cc8f53fd3a2aa6dab0824e78b714b0ee5a
Ming Wu <mw4086@rit.edu>
2021-09-26 15:42:11 -0400

    q12

--- Unit1Test1Q12/Program.cs
+++ Unit1Test1Q12/Program.cs
@@ -8,9 +8,25 @@ namespace Unit1Test1Q12
 {
     class Program
     {
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name == "ming")
+            {
+                salary = Math.Round(salary + 19999.99, 2);
+                return true;
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-
+            string sName;
+            double dSalary = 30000;
+            Console.Write("What's your name: ");
+            sName = Console.ReadLine();
+            if (GiveRaise(sName, ref dSalary))
+            {
+                Console.WriteLine("Congrats, you got a raise, your new salary is: {0}", dSalary);
+            }
         }
     }
 }
