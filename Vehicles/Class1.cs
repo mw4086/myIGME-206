84547b8829cf714bb8eae08e51baf6c64de165d8
Ming Wu <mw4086@rit.edu>
2021-09-27 19:55:56 -0400

    fix things up

--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -6,61 +6,58 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
-    class Program
+    public abstract class Vehicle
     {
-        public abstract class Vehicle
-        {
-            public virtual void LoadPassenger(){ }
-        }
+        public virtual void LoadPassenger(){ }
+    }
 
-        public interface IPassengerCarrier
-        {
-            void LoadPassenger();
-        }
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
 
-        public interface IHeavyLoadCarrier
-        {
+    public interface IHeavyLoadCarrier
+    {
 
-        }
+    }
 
-        public abstract class Car : Vehicle
-        {
+    public abstract class Car : Vehicle
+    {
 
-        }
+    }
 
-        public abstract class Train : Vehicle
-        {
+    public abstract class Train : Vehicle
+    {
 
-        }
+    }
 
-        public class Compact : Car, IPassengerCarrier
-        {
+    public class Compact : Car, IPassengerCarrier
+    {
 
-        }
+    }
 
-        public class SUV : Car, IPassengerCarrier
-        {
+    public class SUV : Car, IPassengerCarrier
+    {
 
-        }
+    }
 
-        public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
-        {
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
 
-        }
+    }
 
-        public class PassengerTrain : Train, IPassengerCarrier
-        {
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
 
-        }
+    }
 
-        public class FreightTrain : Train, IHeavyLoadCarrier
-        {
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
 
-        }
+    }
 
-        public class _424DoubleBogey : Train, IHeavyLoadCarrier
-        {
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
 
-        }
     }
 }
