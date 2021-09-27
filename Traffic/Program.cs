0df76438e2d608a24c9c3d5a12c580eacb4b0315
Ming Wu <mw4086@rit.edu>
2021-09-27 18:38:38 -0400

    unfixed

---
+++ Traffic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    class Program
+    {
+        public static void AddPassenger(IPassengerCarrier obj)
+        {
+                obj.LoadPassenger();
+                Console.WriteLine(obj.ToString());
+        }
+    }
+}
