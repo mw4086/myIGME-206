8a0e7b87411ab57053f08e4761a13c635cd6a55b
Ming Wu <mw4086@rit.edu>
2021-10-01 16:29:24 -0400

    PE12Q3

---
+++ PE12Q3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12Q3
+{
+    class MyClass
+    {
+
+            private string myString;
+            public virtual string GetString()
+        {
+            return myString;
+        }
+        class MyDerivedClass : MyClass
+        {
+            public override string GetString()
+            {
+                return base.GetString() + " (output from derived class)";
+            }
+        }
+    }
+}
