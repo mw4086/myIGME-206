09d23767b39fe4651e5f89a24582e03d5f78ad70
Ming Wu <mw4086@rit.edu>
2021-10-05 22:45:03 -0400

    PE13

---
+++ PE13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE13
+{
+    class Program
+    {
+        //2
+        public class Class1 : IInterface
+        {
+            public void aMethod()
+            {
+                Console.WriteLine("Something from Class1");
+            }
+        }
+
+        public class Class2 : IInterface
+        {
+            public void aMethod()
+            {
+                Console.WriteLine("Something from Class2");
+            }
+        }
+
+        //3
+        public interface IInterface
+        {
+            void aMethod();
+        }
+        public static void MyMethod(object myObject)
+        {
+            IInterface iInterface = (IInterface)myObject;
+            iInterface.aMethod();
+        }
+
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Class2 class2 = new Class2();
+            MyMethod(class1);
+            MyMethod(class2);
+        }
+    }
+}
+
